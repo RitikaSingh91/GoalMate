ad06f11fcf7367a20bf589a9aa365e5591d976d5
RitikaSingh91 <ritikasinghharshjeetsingh@gmail.com>
2025-12-03 23:43:32 +0530

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Title: Today’s Target 🎯</h1>
+      <p>Content:A fresh day brings new opportunities — grab them!</p>
+    </div>
+  );
+}
+
+export default Note;
